12bc6f7b1ed503c66d708935abee115fef928bb9
SergeAuck <sergey.auckland@gmail.com>
2023-06-27 19:39:38 +1200

    initial commit

---
+++ Bit504AS3breakout/src/Paddle.java
@@ -0,0 +1,44 @@
+import java.awt.Graphics;
+
+public class Paddle extends Sprite {
+
+	private int xVelocity;
+	
+	public Paddle() {
+		// TODO: Set width to Settings.PADDLE_WIDTH
+		setWidth(Settings.PADDLE_WIDTH);
+		// TODO: Set width to Settings.PADDLE_HEIGHT  height not width
+		setHeight(Settings.PADDLE_HEIGHT);
+		// TODO: Call resetPosition
+		resetPosition();
+	}
+	
+	public void resetPosition() {
+		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
+		setX(Settings.INITIAL_PADDLE_X);
+		setY(Settings.INITIAL_PADDLE_Y);
+		// Note: Check Ball.java for a hint
+	}
+	
+	public void update() {
+		x += xVelocity;
+		// TODO: Prevent the paddle from moving outside of the screen
+		// This can be done using two if statements (one for the left side of the screen and one for the right)
+		if (x  >= Settings.WINDOW_WIDTH -  Settings.PADDLE_WIDTH -15) { //extra -15 to stop paddle exactly on the border
+			x = Settings.WINDOW_WIDTH - Settings.PADDLE_WIDTH - 15;
+		}
+		if (x <= 0) {
+			x = 0;
+		}
+
+	}
+	
+	public void paint(Graphics g) {
+		g.fillRect(x, y, Settings.PADDLE_WIDTH, Settings.PADDLE_HEIGHT);
+	}
+	
+	public void setXVelocity(int vel) {
+		// TODO: Set x velocity
+		xVelocity = vel;
+	}
+}
